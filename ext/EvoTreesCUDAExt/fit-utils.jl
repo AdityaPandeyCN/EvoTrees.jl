fd5590395a43d29baf2f54587d45a2c7b92dafbd
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-09-03 13:51:06 +0530

    changes
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- ext/EvoTreesCUDAExt/fit-utils.jl
+++ ext/EvoTreesCUDAExt/fit-utils.jl
@@ -100,8 +100,16 @@ end
             
             for k in 1:(2*K+1)
                 sum_val = zero(T)
+<<<<<<< HEAD
                 for j_idx in 1:length(js), b in 1:nbins
                     sum_val += h∇[k, b, j_idx, node]
+=======
+                for j_idx in 1:length(js)
+                    f = js[j_idx]
+                    for b in 1:nbins
+                        sum_val += h∇[k, b, f, node]
+                    end
+>>>>>>> e03c8d4 (changes)
                 end
                 nodes_sum[k, node] = sum_val
             end
@@ -123,6 +131,7 @@ end
                 f = js[j_idx]
                 is_numeric = feattypes[f]
                 constraint = monotone_constraints[f]
+<<<<<<< HEAD
 
                 if is_numeric
                     cum_w = zero(T)
@@ -164,14 +173,74 @@ end
                                 if g > g_best
                                     g_best, b_best, f_best = g, Int32(b), Int32(f)
                                 end
+=======
+                
+                if is_numeric  
+                    for b in 1:(nbins - 1)
+                        s_w = zero(T)
+                        gain_l = zero(T)
+                        gain_r = zero(T)
+                        predL = zero(T)
+                        predR = zero(T)
+                        @inbounds for kk in 1:K
+                            # cumulative sums up to bin b for numeric features
+                            l_g = zero(T)
+                            l_h = zero(T)
+                            @inbounds for bb in 1:b
+                                l_g += h∇[kk, bb, f, node]
+                                l_h += h∇[K+kk, bb, f, node]
+                            end
+                            # weight cumulative
+                            # compute once outside kk loop; but safe to accumulate here
+                            # will be overwritten by final value after kk loop
+                        end
+                        # compute weight cumulative once
+                        @inbounds for bb in 1:b
+                            s_w += h∇[2*K+1, bb, f, node]
+                        end
+                        if s_w >= min_weight && (w_p - s_w) >= min_weight
+                            @inbounds for kk in 1:K
+                                l_g = zero(T)
+                                l_h = zero(T)
+                                @inbounds for bb in 1:b
+                                    l_g += h∇[kk, bb, f, node]
+                                    l_h += h∇[K+kk, bb, f, node]
+                                end
+                                r_g = nodes_sum[kk, node] - l_g
+                                r_h = nodes_sum[K+kk, node] - l_h
+                                denomL = l_h + lambda * (s_w / K) + T(1e-8)
+                                denomR = r_h + lambda * ((w_p - s_w) / K) + T(1e-8)
+                                gain_l += l_g^2 / denomL
+                                gain_r += r_g^2 / denomR
+                                if constraint != 0
+                                    predL += -l_g / denomL
+                                    predR += -r_g / denomR
+                                end
+                            end
+                            if constraint != 0
+                                if !((constraint == 0) || (constraint == -1 && predL > predR) || (constraint == 1 && predL < predR))
+                                    continue
+                                end
+                            end
+                            g = gain_l + gain_r - gain_p
+                            if g > g_best
+                                g_best = g
+                                b_best = Int32(b)
+                                f_best = Int32(f)
+>>>>>>> e03c8d4 (changes)
                             end
                         end
                     end
                 else
                     for b in 1:(nbins - 1)
+<<<<<<< HEAD
                         l_w = h∇[2*K+1, b, j_idx, node]
+=======
+                        l_w = h∇[2*K+1, b, f, node]
+>>>>>>> e03c8d4 (changes)
                         r_w = w_p - l_w
                         if l_w >= min_weight && r_w >= min_weight
+<<<<<<< HEAD
                             gain_valid = true
                             if constraint != 0
                                 l_g1 = h∇[1, b, j_idx, node]
@@ -199,6 +268,36 @@ end
                                 if g > g_best
                                     g_best, b_best, f_best = g, Int32(b), Int32(f)
                                 end
+=======
+                            gain_l = zero(T)
+                            gain_r = zero(T)
+                            predL = zero(T)
+                            predR = zero(T)
+                            @inbounds for kk in 1:K
+                                l_g = h∇[kk, b, f, node]
+                                l_h = h∇[K+kk, b, f, node]
+                                r_g = nodes_sum[kk, node] - l_g
+                                r_h = nodes_sum[K+kk, node] - l_h
+                                denomL = l_h + lambda * (l_w / K) + T(1e-8)
+                                denomR = r_h + lambda * (r_w / K) + T(1e-8)
+                                gain_l += l_g^2 / denomL
+                                gain_r += r_g^2 / denomR
+                                if constraint != 0
+                                    predL += -l_g / denomL
+                                    predR += -r_g / denomR
+                                end
+                            end
+                            if constraint != 0
+                                if !((constraint == 0) || (constraint == -1 && predL > predR) || (constraint == 1 && predL < predR))
+                                    continue
+                                end
+                            end
+                            g = gain_l + gain_r - gain_p
+                            if g > g_best
+                                g_best = g
+                                b_best = Int32(b)
+                                f_best = Int32(f)
+>>>>>>> e03c8d4 (changes)
                             end
                         end
                     end
