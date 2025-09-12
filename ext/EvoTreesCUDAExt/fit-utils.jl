eaf6fdd2ac83a0fa9abcecaaa0823d0cbcf69528
AdityaPandeyCN <adityapand3y666@gmail.com>
2025-09-12 18:10:42 +0530

    d
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- ext/EvoTreesCUDAExt/fit-utils.jl
+++ ext/EvoTreesCUDAExt/fit-utils.jl
@@ -29,6 +29,16 @@ const MAX_K = 8
     end
 end
 
+@kernel function create_mask_kernel!(mask::AbstractVector{UInt8}, @Const(nodes))
+    i = @index(Global)
+    @inbounds if i <= length(nodes)
+        node = nodes[i]
+        if node > 0 && node <= length(mask)
+            mask[node] = UInt8(1)
+        end
+    end
+end
+
 @kernel function hist_kernel!(
     h∇::AbstractArray{T,4},
     @Const(∇),
@@ -36,29 +46,32 @@ end
     @Const(nidx),
     @Const(js),
     @Const(is),
-    K::Int
+    @Const(mask),
+    K::Int,
+    is_mle2p::Bool
 ) where {T}
     gidx = @index(Global, Linear)
     n_feats = length(js)
     n_obs = length(is)
     obs_per_thread = 64
-    
+
     total_work = cld(n_obs, obs_per_thread) * n_feats
     if gidx <= total_work
         feat_idx = (gidx - 1) % n_feats + 1
-        obs_chunk = (gidx - 1) ÷ n_feats
+        obs_chunk_idx = (gidx - 1) ÷ n_feats
         feat = js[feat_idx]
-        
-        start_idx = obs_chunk * obs_per_thread + 1
+
+        start_idx = obs_chunk_idx * obs_per_thread + 1
         end_idx = min(start_idx + obs_per_thread - 1, n_obs)
-        
-        @inbounds for obs_idx in start_idx:end_idx
-            obs = is[obs_idx]
+
+        @inbounds for i_obs in start_idx:end_idx
+            obs = is[i_obs]
             node = nidx[obs]
-            if node > 0 && node <= size(h∇, 4)
+            if node > 0 && node <= length(mask) && mask[node] == 1
                 bin = x_bin[obs, feat]
                 if bin > 0 && bin <= size(h∇, 2)
-                    for k in 1:(2*K+1)
+                    n_grad_hess = is_mle2p ? 5 : (2 * K + 1)
+                    for k in 1:n_grad_hess
                         Atomix.@atomic h∇[k, bin, feat, node] += ∇[k, obs]
                     end
                 end
@@ -90,161 +103,94 @@ end
         node = active_nodes[n_idx]
         if node == 0
             gains[n_idx], bins[n_idx], feats[n_idx] = T(-Inf), Int32(0), Int32(0)
-        else
-            nbins = size(h∇, 2)
-            eps = T(1e-8)
-            
-            @inbounds begin
-                local_f = js[1]
-                n_grad_hess = is_mle2p ? 5 : (2 * K + 1)
-                for k in 1:n_grad_hess
-                    total = zero(T)
-                    for b in 1:nbins
-                        total += T(h∇[k, b, local_f, node])
-                    end
-                    nodes_sum[k, node] = total
-                end
-            end
-            
-            w_p = is_mle2p ? nodes_sum[5, node] : nodes_sum[2*K+1, node]
-            g_best, b_best, f_best = T(-Inf), Int32(0), Int32(0)
-            
-            if is_mae || is_quantile
-                parent_g = nodes_sum[1, node]
-                parent_h = nodes_sum[2, node]
-                
-                gain_p = parent_g^2 / (parent_h + lambda * w_p + L2 + eps) / 2
-                
-                for j_idx in 1:length(js)
-                    f = js[j_idx]
-                    is_numeric = feattypes[f]
-                    
-                    bin_range = is_numeric ? (1:(nbins-1)) : (1:(nbins-1))
-                    s_w = zero(T)
-                    cum_g = zero(T)
-                    cum_h = zero(T)
-                    
-                    for b in bin_range
-                        if is_numeric
-                            s_w += h∇[2*K+1, b, f, node]
-                            cum_g += h∇[1, b, f, node]
-                            cum_h += h∇[2, b, f, node]
-                        else
-                            s_w = h∇[2*K+1, b, f, node]
-                            cum_g = h∇[1, b, f, node]
-                            cum_h = h∇[2, b, f, node]
-                        end
-                        
-                        l_w, r_w = s_w, w_p - s_w
-                        if l_w >= min_weight && r_w >= min_weight
-                            l_g, r_g = cum_g, parent_g - cum_g
-                            l_h, r_h = cum_h, parent_h - cum_h
-                            
-                            gain_l = l_g^2 / (l_h + lambda * l_w + L2 + eps) / 2
-                            gain_r = r_g^2 / (r_h + lambda * r_w + L2 + eps) / 2
-                            g = gain_l + gain_r - gain_p
-                            
-                            if g > g_best
-                                g_best, b_best, f_best = g, Int32(b), Int32(f)
-                            end
-                        end
-                    end
-                end
-            else
-                gain_p = zero(T)
-                if is_mle2p && K == 2
-                    g1, g2 = nodes_sum[1, node], nodes_sum[2, node]
-                    h1, h2 = nodes_sum[3, node], nodes_sum[4, node]
-                    gain_p = (g1^2 / (h1 + lambda * w_p + L2 + eps) + g2^2 / (h2 + lambda * w_p + L2 + eps)) / 2
-                else
-                    w = nodes_sum[2*K+1, node]
-                    if w > eps
-                        for kk in 1:K
-                            g = nodes_sum[kk, node]
-                            h = nodes_sum[K+kk, node]
-                            gain_p += g^2 / (h + lambda * w + L2 + eps)
-                        end
-                        gain_p /= 2
-                    end
-                end
-                
-                for j_idx in 1:length(js)
-                    f = js[j_idx]
-                    is_numeric = feattypes[f]
-                    constraint = monotone_constraints[f]
-                    
-                    bin_range = is_numeric ? (1:(nbins-1)) : (1:(nbins-1))
-                    s_w = zero(T)
-                    cum_g = MVector{MAX_K,T}(ntuple(_->zero(T), MAX_K))
-                    cum_h = MVector{MAX_K,T}(ntuple(_->zero(T), MAX_K))
-                    
-                    for b in bin_range
-                        if is_numeric
-                            s_w += is_mle2p ? T(h∇[5, b, f, node]) : T(h∇[2*K+1, b, f, node])
-                            @inbounds for kk in 1:K
-                                cum_g[kk] += T(h∇[kk, b, f, node])
-                                if is_mle2p
-                                    cum_h[kk] += T(h∇[kk+2, b, f, node])
-                                else
-                                    cum_h[kk] += T(h∇[K+kk, b, f, node])
-                                end
-                            end
-                        else
-                            s_w = is_mle2p ? T(h∇[5, b, f, node]) : T(h∇[2*K+1, b, f, node])
-                            @inbounds for kk in 1:K
-                                cum_g[kk] = T(h∇[kk, b, f, node])
-                                if is_mle2p
-                                    cum_h[kk] = T(h∇[kk+2, b, f, node])
-                                else
-                                    cum_h[kk] = T(h∇[K+kk, b, f, node])
-                                end
-                            end
-                        end
-                        
-                        if s_w >= min_weight && (w_p - s_w) >= min_weight
-                            gain_l_unscaled, gain_r_unscaled = zero(T), zero(T)
-                            predL, predR = zero(T), zero(T)
-                            
-                            @inbounds for kk in 1:K
-                                l_g, l_h = cum_g[kk], cum_h[kk]
-                                r_w = w_p - s_w
-
-                                if is_mle2p
-                                    r_g, r_h = nodes_sum[kk, node] - l_g, nodes_sum[kk+2, node] - l_h
-                                else
-                                    r_g, r_h = nodes_sum[kk, node] - l_g, nodes_sum[K+kk, node] - l_h
-                                end
-
-                                denomL = l_h + lambda * s_w + L2 + eps
-                                denomR = r_h + lambda * r_w + L2 + eps
-                                
-                                gain_l_unscaled += l_g^2 / denomL
-                                gain_r_unscaled += r_g^2 / denomR
-
-                                if constraint != 0 && (!is_mle2p || kk == 1)
-                                    predL += -l_g / denomL
-                                    predR += -r_g / denomR
-                                end
-                            end
-                            
-                            constraint_ok = (constraint == 0) || 
-                                           (constraint == -1 && predL > predR) || 
-                                           (constraint == 1 && predL < predR)
-
-                            if constraint_ok
-                                gain_l = gain_l_unscaled / 2
-                                gain_r = gain_r_unscaled / 2
-                                g = gain_l + gain_r - gain_p
-                                if g > g_best
-                                    g_best, b_best, f_best = g, Int32(b), Int32(f)
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-            gains[n_idx], bins[n_idx], feats[n_idx] = g_best, b_best, f_best
+            return
         end
+
+        nbins = size(h∇, 2)
+        eps = T(1e-8)
+
+        @inbounds begin
+            local_f = js[1]
+            n_grad_hess = is_mle2p ? 5 : (2 * K + 1)
+            for k in 1:n_grad_hess
+                total = zero(T)
+                for b in 1:nbins
+                    total += T(h∇[k, b, local_f, node])
+                end
+                nodes_sum[k, node] = total
+            end
+        end
+
+        w_p = is_mle2p ? nodes_sum[5, node] : nodes_sum[2*K+1, node]
+        g_best, b_best, f_best = T(-Inf), Int32(0), Int32(0)
+
+        if w_p < 2 * min_weight
+            gains[n_idx], bins[n_idx], feats[n_idx] = g_best, b_best, f_best
+            return
+        end
+
+        parent_gain = zero(T)
+        if is_mle2p && K == 2
+            g1, g2 = nodes_sum[1, node], nodes_sum[2, node]
+            h1, h2 = nodes_sum[3, node], nodes_sum[4, node]
+            parent_gain = g1^2 / (h1 + lambda * w_p + L2 + eps) + g2^2 / (h2 + lambda * w_p + L2 + eps)
+        else
+            for kk in 1:K
+                g = nodes_sum[kk, node]
+                h = nodes_sum[K+kk, node]
+                parent_gain += g^2 / (h + lambda * w_p + L2 + eps)
+            end
+        end
+
+        for j_idx in 1:length(js)
+            f = js[j_idx]
+            constraint = monotone_constraints[f]
+            s_w = zero(T)
+            cum_g = MVector{MAX_K,T}(ntuple(_ -> zero(T), MAX_K))
+            cum_h = MVector{MAX_K,T}(ntuple(_ -> zero(T), MAX_K))
+
+            for b in 1:(nbins-1)
+                s_w += is_mle2p ? T(h∇[5, b, f, node]) : T(h∇[2*K+1, b, f, node])
+                for kk in 1:K
+                    cum_g[kk] += T(h∇[kk, b, f, node])
+                    cum_h[kk] += is_mle2p ? T(h∇[kk+2, b, f, node]) : T(h∇[K+kk, b, f, node])
+                end
+
+                if s_w >= min_weight && (w_p - s_w) >= min_weight
+                    left_gain, right_gain = zero(T), zero(T)
+                    predL, predR = zero(T), zero(T)
+                    
+                    for kk in 1:K
+                        l_g, l_h = cum_g[kk], cum_h[kk]
+                        r_w = w_p - s_w
+                        r_g = nodes_sum[kk, node] - l_g
+                        r_h = (is_mle2p ? nodes_sum[kk+2, node] : nodes_sum[K+kk, node]) - l_h
+                        
+                        denomL = l_h + lambda * s_w + L2 + eps
+                        denomR = r_h + lambda * r_w + L2 + eps
+                        
+                        left_gain += l_g^2 / denomL
+                        right_gain += r_g^2 / denomR
+
+                        if constraint != 0 && (!is_mle2p || kk == 1)
+                            predL += -l_g / denomL
+                            predR += -r_g / denomR
+                        end
+                    end
+                    
+                    constraint_ok = (constraint == 0) || (constraint == -1 && predL > predR) || (constraint == 1 && predL < predR)
+                    
+                    if constraint_ok
+                        g = left_gain + right_gain - parent_gain
+                        if g > g_best
+                            g_best, b_best, f_best = g, Int32(b), Int32(f)
+                        end
+                    end
+                end
+            end
+        end
+        g_best /= 2
+        gains[n_idx], bins[n_idx], feats[n_idx] = g_best, b_best, f_best
     end
 end
 
@@ -268,48 +214,49 @@ end
     end
 end
 
-@kernel function subtract_hist_kernel!(h∇::AbstractArray{T,4}, @Const(subtract_nodes), n_k, n_b, n_j) where {T}
-    gidx = @index(Global)
-    n_elements = n_k * n_b * n_j
+@kernel function subtract_hist_kernel!(
+    h∇::AbstractArray{T,4},
+    @Const(h∇_parent),
+    @Const(subtract_nodes)
+) where {T}
+    gidx = @index(Global, Linear)
+    n_k, n_b, n_j = size(h∇, 1), size(h∇, 2), size(h∇, 3)
+    n_elements_per_node = n_k * n_b * n_j
     
-    node_idx = (gidx - 1) ÷ n_elements + 1
-    if node_idx <= length(subtract_nodes)
-        @inbounds node = subtract_nodes[node_idx]
+    node_idx = (gidx - 1) ÷ n_elements_per_node + 1
+
+    @inbounds if node_idx <= length(subtract_nodes)
+        node = subtract_nodes[node_idx]
         if node > 0
             parent = node >> 1
-            sibling = node ⊻ 1
+            sibling = node - 1
             
-            elem_idx = (gidx - 1) % n_elements
-            j = elem_idx ÷ (n_k * n_b) + 1
-            remainder = elem_idx % (n_k * n_b)
-            b = remainder ÷ n_k + 1
-            k = remainder % n_k + 1
+            element_idx_in_node = (gidx - 1) % n_elements_per_node + 1
             
-            h∇[k, b, j, node] = h∇[k, b, j, parent] - h∇[k, b, j, sibling]
+            h∇[element_idx_in_node, node] = h∇_parent[element_idx_in_node, parent] - h∇[element_idx_in_node, sibling]
         end
     end
 end
 
 function update_hist_gpu!(
-    h∇, gains, bins, feats, ∇, x_bin, nidx, js, is, depth, active_nodes,
-    nodes_sum_gpu, params, left_nodes_buf, right_nodes_buf, target_mask_buf,
+    h∇, h∇_parent,
+    gains, bins, feats, ∇, x_bin, nidx, js, is, depth, active_nodes,
+    nodes_sum_gpu, params, left_nodes_buf, right_nodes_buf, build_mask,
     feattypes, monotone_constraints, K;
-    is_mae::Bool=false, is_quantile::Bool=false, is_cred::Bool=false, is_mle2p::Bool=false
+    is_mae::Bool=false, is_quantile::Bool=false, is_mle2p::Bool=false
 )
     backend = KernelAbstractions.get_backend(h∇)
     n_active = length(active_nodes)
     
-    h∇ .= 0
-    
-    n_work = cld(length(is), 64) * length(js)
-    workgroup_size = min(256, n_work)
-    hist_kernel!(backend)(
-        h∇, ∇, x_bin, nidx, js, is, K;
-        ndrange = n_work,
-        workgroupsize = workgroup_size
-    )
-    
-    if n_active > 16 && depth > 2
+    if depth == 1
+        h∇ .= 0
+        build_mask .= 1
+        hist_kernel!(backend)(
+            h∇, ∇, x_bin, nidx, js, is, build_mask, K, is_mle2p;
+            ndrange = cld(length(is), 64) * length(js),
+            workgroupsize = 256
+        )
+    else
         build_count = KernelAbstractions.zeros(backend, Int32, 1)
         subtract_count = KernelAbstractions.zeros(backend, Int32, 1)
         
@@ -317,15 +264,29 @@ function update_hist_gpu!(
             left_nodes_buf, build_count, right_nodes_buf, subtract_count, active_nodes;
             ndrange = n_active, workgroupsize = min(256, n_active)
         )
-        
         KernelAbstractions.synchronize(backend)
-        
+        n_build = Array(build_count)[1]
         n_subtract = Array(subtract_count)[1]
+
+        if n_build > 0
+            build_mask .= 0
+            build_nodes_view = view(left_nodes_buf, 1:n_build)
+            create_mask_kernel!(backend)(build_mask, build_nodes_view; ndrange=n_build, workgroupsize=min(256, n_build))
+
+            h∇ .= 0
+            hist_kernel!(backend)(
+                h∇, ∇, x_bin, nidx, js, is, build_mask, K, is_mle2p;
+                ndrange = cld(length(is), 64) * length(js),
+                workgroupsize = 256
+            )
+        end
+
         if n_subtract > 0
-            n_k, n_b, n_j = size(h∇, 1), size(h∇, 2), size(h∇, 3)
+            subtract_nodes_view = view(right_nodes_buf, 1:n_subtract)
+            n_elems_per_node = size(h∇, 1) * size(h∇, 2) * size(h∇, 3)
             subtract_hist_kernel!(backend)(
-                h∇, view(right_nodes_buf, 1:n_subtract), n_k, n_b, n_j;
-                ndrange = n_subtract * n_k * n_b * n_j,
+                h∇, h∇_parent, subtract_nodes_view;
+                ndrange = n_subtract * n_elems_per_node,
                 workgroupsize = 256
             )
         end
@@ -334,13 +295,8 @@ function update_hist_gpu!(
     find_best_split_from_hist_kernel!(backend)(
         gains, bins, feats, h∇, nodes_sum_gpu, active_nodes, js,
         feattypes, monotone_constraints,
-        eltype(gains)(params.lambda),
-        eltype(gains)(params.min_weight),
-        eltype(gains)(params.L2),
-        K,
-        is_mae,
-        is_quantile,
-        is_mle2p;
+        eltype(gains)(params.lambda), eltype(gains)(params.min_weight), eltype(gains)(params.L2),
+        K, is_mae, is_quantile, is_mle2p;
         ndrange = n_active,
         workgroupsize = min(256, n_active)
     )
